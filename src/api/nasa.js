1c9544d9e84a2fe52b0e1a0a95bbab78ab4ba165
anishakujur367 -alt <anishakujur367@gmail.com>
2026-03-31 13:29:12 +0530

    First Commit

--- src/api/nasa.js
+++
@@ -1,9 +0,0 @@
-const fetchAPOD = async () => {
-  const res = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=h0u5kE9e8hQuSV5sDufXdelTm7Jyh9M1zqG2hg5t`
-  );
-  const data = await res.json();
-  return data;
-};
-
-export default fetchAPOD;
